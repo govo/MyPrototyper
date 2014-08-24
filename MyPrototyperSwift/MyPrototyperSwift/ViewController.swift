a7c250eb2fb5d82a718b23094609da4f08b21523
govo <ejialin@gmail.com>
2014-08-24 21:57:21 +0800

    swift language

---
+++ MyPrototyperSwift/MyPrototyperSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPrototyperSwift
+//
+//  Created by govo on 14-8-24.
+//  Copyright (c) 2014年 info.govo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
