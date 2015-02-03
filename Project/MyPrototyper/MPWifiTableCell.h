60dc7dfe41b78a329dbf7be9fdc67d2cd560e20d
govo <ejialin@gmail.com>
2015-02-04 01:53:45 +0800

    添加wifi图片及view

---
+++ Project/MyPrototyper/MPWifiTableCell.h
@@ -0,0 +1,13 @@
+//
+//  MPWifiTableCell.h
+//  MyPrototyper
+//
+//  Created by govo on 15/2/4.
+//  Copyright (c) 2015年 me.govo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPWifiTableCell : UITableViewCell
+
+@end
---
+++ Project/MyPrototyper/MPWifiTableCell.m
@@ -0,0 +1,23 @@
+//
+//  MPWifiTableCell.m
+//  MyPrototyper
+//
+//  Created by govo on 15/2/4.
+//  Copyright (c) 2015年 me.govo. All rights reserved.
+//
+
+#import "MPWifiTableCell.h"
+
+@implementation MPWifiTableCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
